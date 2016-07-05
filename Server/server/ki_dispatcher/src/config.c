e8ccf7370f23520f9acb95e5ec90d7a26737c944
deyou <deyou@99b53355-9361-43ae-ae31-b7af83c9dd04>
2016-07-05 05:49:40 +0000

    update the ip

--- Server/server/ki_dispatcher/src/config.c
+++ Server/server/ki_dispatcher/src/config.c
@@ -6,22 +6,22 @@ config_t config_init(){
 	config_t t = r_calloc(1,sizeof(struct config_st));
 
 	// init the imserver_push ip
-	strcpy(t->imserver_push_ip_addr,"tcp://121.42.207.163:22222");
+	strcpy(t->imserver_push_ip_addr,"tcp://10.174.244.27:22222");
 
 	// init the imserver_pull ip
-	strcpy(t->imserver_pull_ip_addr,"tcp://121.42.207.163:22221");
+	strcpy(t->imserver_pull_ip_addr,"tcp://10.174.244.27:22221");
 
 	// init the schat push ip
-	strcpy(t->schat_push_ip_addr,"tcp://121.42.207.163:22226");
+	strcpy(t->schat_push_ip_addr,"tcp://10.174.244.27:22226");
 
 	// init the schat pull ip
-	strcpy(t->schat_pull_ip_addr,"tcp://121.42.207.163:22225");
+	strcpy(t->schat_pull_ip_addr,"tcp://10.174.244.27:22225");
 
 	// init the imserver ip
-	strcpy(t->imserver_ip,"121.42.207.163");
+	strcpy(t->imserver_ip,"10.174.244.27");
 
 	// init the memcacheq ip
-	strcpy(t->memcacheq_server, "121.42.207.163");
+	strcpy(t->memcacheq_server, "10.174.244.27");
 	t->memcacheq_port = 11212;
 
 	// init the schat topic
